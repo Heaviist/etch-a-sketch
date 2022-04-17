b238af2f18ed7565ec908180cb4b5efe8486bc3c
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-04-17 23:09:53 +0200

    Add etching and viewport size dependent boxes
    Added an eventListener to check if there's a mouseover. The box will
    then transition gently into an etched state.
    
    Viewport height decides the padding size of the box divs.

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const gridSide = 16;
-const container = document.querySelector('#container')
+const container = document.querySelector('#container');
+const viewPort = window.innerHeight;
+const boxSide = (viewPort - (viewPort % gridSide))/gridSide;
+console.log(viewPort, boxSide);
 
 for (let i = 0; i < gridSide; i++) {
   const horizontalContainer = document.createElement('div');
   horizontalContainer.classList.add('horizontal-container');
-  container.appendChild(horizontalContainer);
-
+  container.appendChild(horizontalContainer); //Create horizontal container divs
   for (let j = 0; j < gridSide; j++) {
     const verticalBox = document.createElement('div');
     verticalBox.classList.add('box');
-    horizontalContainer.appendChild(verticalBox);
+    verticalBox.style.padding = `${boxSide/2}px`;
+    horizontalContainer.appendChild(verticalBox); //Append boxes vertically to containers
   }
-
 }
+
+const etchBox = document.querySelectorAll('.box');
+
+etchBox.forEach(box => {
+  box.addEventListener('mouseover', () => {
+    box.classList.add('etched');
+  })
+});
+
+/*etchBox.forEach(box => {
+  box.addEventListener('mouseleave', () => {
+    box.classList.remove('etched');
+  })
+});*/
