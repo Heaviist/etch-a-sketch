a0486ba2d5991eee31a2b95f8a473131a1c62544
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-04-18 02:51:09 +0200

    Simplify reset and rewrite code to functions
    Reset function was very resource heavy. Removed unneeded instructions.
    Reused code written in functions.
    
    Note: after resetting, you need to generate a new etchBox constant
    which contains all the box divs. Otherwise the event listener will not
    have any divs to listen to.

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 let gridSide = 16;
 const container = document.querySelector('#container');
-const viewPort = window.innerHeight-50;
-const boxSide = (viewPort - (viewPort % gridSide))/gridSide;
-console.log(viewPort, boxSide);
+//const viewPort = window.innerHeight-50;
+//const boxSide = (viewPort - (viewPort % gridSide))/gridSide;
+//console.log(viewPort, boxSide);
 
-for (let i = 0; i < gridSide; i++) {
-  const horizontalContainer = document.createElement('div');
-  horizontalContainer.classList.add('horizontal-container');
-  container.appendChild(horizontalContainer); //Create horizontal container divs
-  for (let j = 0; j < gridSide; j++) {
-    const verticalBox = document.createElement('div');
-    verticalBox.classList.add('box');
-    verticalBox.style.padding = `${boxSide/2}px`;
-    horizontalContainer.appendChild(verticalBox); //Append boxes vertically to containers
+createGrid(gridSide);
+
+function createGrid(size) {
+  for (let i = 0; i < size; i++) {
+    const horizontalContainer = document.createElement('div');
+    horizontalContainer.classList.add('horizontal-container');
+    container.appendChild(horizontalContainer); //Create horizontal container divs
+    for (let j = 0; j < size; j++) {
+      const verticalBox = document.createElement('div');
+      verticalBox.classList.add('box');
+      //verticalBox.style.padding = `${boxSide/2}px`;
+      horizontalContainer.appendChild(verticalBox); //Append boxes vertically to containers
+    }
   }
+  draw();
 }
 
-const etchBox = document.querySelectorAll('.box');
+function draw() {
+  const etchBox = document.querySelectorAll('.box');
 
-etchBox.forEach(box => {
-  box.addEventListener('mouseover', () => {
-    box.classList.add('etched');
-  })
-});
+  etchBox.forEach(box => {
+    box.addEventListener('mouseover', () => {
+      box.classList.add('etched');
+    })
+  });
+}
 
 function reset() {
-  etchBox.forEach(box => {
-    box.classList.remove('etched');
-  });
+  container.innerHTML = '';
+  createGrid(gridSide);
 }
